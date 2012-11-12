a7d1ff6cb8df70c091f1610a1504b9af78ddaddb
Michael James <pheonix@pheonixro.com>
2012-11-12 12:15:16 -0600

    Wholly Skin fixes from Sortokk

--- skins/Wholly.lua
+++ skins/Wholly.lua
@@ -3,17 +3,20 @@ local U = unpack(select(2,...))
 local name = "WhollySkin"
 
 local function SkinWhollyFrame()
-   U.SkinFrame(com_mithrandir_whollyFrame)
-   U.SkinCloseButton(com_mithrandir_whollyFrameCloseButton)
-   U.SkinScrollBar(com_mithrandir_whollyFrameScrollBar)
-   local buttons = {
-      "SwitchZoneButton",
-      "PreferencesButton",
-      "SortButton"
-   }
-   for button,_ in pairs(buttons) do
-      U.SkinButton(_G["com_mithrandir_whollyFrame"..button])
-   end
+	U.SkinFrame(com_mithrandir_whollyFrame)
+	U.SkinCloseButton(com_mithrandir_whollyFrameCloseButton)
+	U.SkinScrollBar(com_mithrandir_whollyFrameScrollBar)
+	com_mithrandir_whollyFrameZoneButton:ClearAllPoints()
+	com_mithrandir_whollyFrameZoneButton:Point("TOP",com_mithrandir_whollyFrame,"TOP",60,-40)
+
+	local buttons = {
+		"SwitchZoneButton",
+		"PreferencesButton",
+		"SortButton"
+	}
+	for button,_ in pairs(buttons) do
+		U.SkinButton(_G["com_mithrandir_whollyFrame"..button])
+	end
 end
 
 local function SkinWholly(self)
