0efec7b8e92f641a3d20332d0aedba7376954d44
Michael James <pheonix@pheonixro.com>
2012-11-12 00:38:59 -0600

    Init Update

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- init for Tukui_ElvUI_Skins
 
-if not (IsAddOnLoaded("ElvUI") or IsAddOnLoaded("Tukui")) then return end
 local addon,Engine = ...
 
 local AddOn = {};
@@ -11,17 +10,9 @@ UISkins = Engine
 AddOn.FrameLocks = {}
 
 local s
-if ElvUI then
-	local E, L, V, P, G, DF = unpack(ElvUI)
-	local S = E:GetModule('Skins')
-	AddOn.s = S
-	AddOn.c = E
-	AddOn.ccolor = E.myclass
-	s = S
-else
-	local T, C, L = unpack(Tukui)
-	AddOn.s = T
-	AddOn.c = C
-	AddOn.ccolor = T.myclass
-	s = T
-end
+
+local T, C, L = unpack(Tukui)
+AddOn.s = T
+AddOn.c = C
+AddOn.ccolor = T.myclass
+s = T
