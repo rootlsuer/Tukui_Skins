d05a9c88a87980da92fb0d6c387450ab04daf7d7
Michael James <pheonix@pheonixro.com>
2012-11-12 14:43:35 -0600

    tCombo Skin Update

--- skins/tCombo.lua
+++ skins/tCombo.lua
@@ -3,8 +3,12 @@ local U = unpack(select(2,...))
 local name = 'tComboSkin'
 local function SkintCombo(self)
 	cDelay(1,function()
-		tComboPoints:SetTemplate("Transparent")
-		tComboEnergyBar:SetTemplate("Transparent")
+		if tComboPoints then
+			tComboPoints:SetTemplate("Transparent")
+		end
+		if tComboEnergyBar then
+			tComboEnergyBar:SetTemplate("Transparent")
+		end
 	end)
 end
 U.RegisterSkin(name,SkintCombo)
