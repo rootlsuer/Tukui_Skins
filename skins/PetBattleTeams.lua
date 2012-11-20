51034ad9f2b0baeb63459929cdafd8c8cda6bde7
Michael James <pheonix@pheonixro.com>
2012-11-19 23:57:42 -0600

    PetBattleTeams Tooltip.

--- skins/PetBattleTeams.lua
+++ skins/PetBattleTeams.lua
@@ -17,6 +17,22 @@ local function SkinPetBattleTeams(self)
 			if _G["PetTeam"..i.."Button2"] then U.SkinFrame(_G["PetTeam"..i.."Button2"], true) _G["PetTeam"..i.."Button2"].BorderAlive:SetTexture(nil) _G["PetTeam"..i.."Button2"].BorderDead:SetTexture(nil) end
 			if _G["PetTeam"..i.."Button3"] then U.SkinFrame(_G["PetTeam"..i.."Button3"], true) _G["PetTeam"..i.."Button3"].BorderAlive:SetTexture(nil) _G["PetTeam"..i.."Button3"].BorderDead:SetTexture(nil) end
 		end
+
+		PetBattleTeamTooltip:HookScript("OnShow", function(self)
+			self.Icon:SetTexCoord(0.12, 0.88, 0.12, 0.88)
+			self.rarityGlow:SetTexture(nil)
+			self.Background:SetTexture(nil)
+			self.BorderTop:SetTexture(nil)
+			self.BorderTopLeft:SetTexture(nil)
+			self.BorderTopRight:SetTexture(nil)
+			self.BorderLeft:SetTexture(nil)
+			self.BorderRight:SetTexture(nil)
+			self.BorderBottom:SetTexture(nil)
+			self.BorderBottomRight:SetTexture(nil)
+			self.BorderBottomLeft:SetTexture(nil)
+			U.SkinFrame(self, true)
+		end)
+
 end
 
 U.RegisterSkin(name,SkinPetBattleTeams)
