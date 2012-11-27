974474660ddae0cbba40ec54bfb4cfb6aa32bd83
Michael James <pheonix@pheonixro.com>
2012-11-27 03:07:55 -0600

    ToC Update

--- skins/WoWPro.lua
+++ skins/WoWPro.lua
@@ -2,7 +2,6 @@ local U = unpack(select(2,...))
 local name = "WoWProSkin"
 
 local function SkinWoWPro(self)
-	print("WoWPro Skinning")
 	U.SkinFrame(Titlebar)
 	U.SkinBackdropFrame(WoWPro.MainFrame)
 	_G["Mouseover Note Tooltip"]:HookScript("OnShow", function(self) self:SetTemplate() end)
