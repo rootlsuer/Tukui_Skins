d3f9155610e3427c6c77c3f87542d2fa2d2af253
Michael James <darktemplar9900@yahoo.com>
2013-08-27 22:02:54 -0500

    xMerchant

--- AddOn/xMerchant.lua
+++ AddOn/xMerchant.lua
@@ -4,8 +4,12 @@ local AS = unpack(select(2,...))
 local name = 'xMerchantSkin'
 function AS:SkinxMerchant()
 	AS:SkinCheckBox(NuuhMerchantFrameTooltipSearching)
-	AS:SkinFrameD(NuuhMerchantFrameSearch)
+	NuuhMerchantFrame:StripTextures(true)
+	AS:SkinFrame(NuuhMerchantFrameSearch, 'Default')
 	AS:SkinScrollBar(NuuhMerchantScrollFrameScrollBar)
+	for i = 1, 10, 1 do
+		AS:SkinTexture(_G['NuuhMerchantFrame'..i].icon)
+	end
 end
 
 AS:RegisterSkin(name, AS.SkinxMerchant)
